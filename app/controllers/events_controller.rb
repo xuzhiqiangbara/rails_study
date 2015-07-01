39552e0fef784b514fb04cff3e13398ce355e3df
xu.zhiqiang <xu.zhiqiang@90202304n.intra.emercury.co.jp>
2015-07-01 17:39:39 +0900

    first

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,57 @@
+class EventsController < ApplicationController
+  before_action :set_event, :only => [ :show, :edit, :update, :destroy]
+
+  def index
+    # @events = Event.all
+    @events = Event.page(params[:page]).per(4)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      # redirect_to :action => :index
+      redirect_to events_url
+      flash[:notice] = "event was successfully created"
+    else
+      render :action => :new
+    end
+  end
+
+  def show
+    @page_title = @event.name
+  end
+
+  def edit
+      @event = Event.find(params[:id])
+  end
+
+  def update
+    if @event.update(event_params)
+      # redirect_to :action => :show, :id => @event
+      redirect_to event_url(@event)
+      flash[:notice] = "event was successfully updated"
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    # redirect_to :action => :index
+    redirect_to events_url
+  end
+
+
+private
+  def event_params
+      params.require(:event).permit(:name, :description)
+  end
+
+  def set_event
+      @event = Event.find(params[:id])
+  end
+end
